910eb60f34b5c9af7fb6db84ac03eee6944437de
DaehoSon <thseogh135@gmail.com>
2023-08-22 13:58:08 +0900

    rename: 프로젝트 패키지명 변경
    - ggongapi -> ggong

--- src/main/java/hdj/ggong/Application.java
+++ src/main/java/hdj/ggong/Application.java
@@ -1,4 +1,4 @@
-package hdj.ggongapi;
+package hdj.ggong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/hdj/ggong/ApplicationTests.java
+++ src/test/java/hdj/ggong/ApplicationTests.java
@@ -1,4 +1,4 @@
-package hdj.ggongapi;
+package hdj.ggong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
